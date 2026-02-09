b794da2214586220afd01259e5a91eda75657bcb
OS-DigitalDev <digitalsamurai.dev@gmail.com>
2026-02-09 23:31:58 +0530

    uploading files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import SearchBar from "./SearchBar";
 import "./Navbar.css";
-import Product from "../pages/Product";
+
 
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
